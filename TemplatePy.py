c747390b3472290c0605a0d06dcd185e76d02aa3
admin <cyril>
2021-01-08 19:54:45 +0800

    111

---
+++ TemplatePy.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+"""
+@author:cyril
+@file:TemplatePy.py
+@time:2021/01/08/18:32
+汉诺塔问题计算
+"""
+
+
+def hanoi(n, a, b, c):
+    if n > 0:
+        hanoi(n - 1, a, c, b)
+        print('move from---%s--to---%s' % (a, c))
+        hanoi(n - 1, b, a, c)
+
+
+if __name__ == '__main__':
+    print('test')
+    hanoi(5, 'a', 'b', 'c')
+    pass
