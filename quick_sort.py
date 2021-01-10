5c4e2e74307e28966116e79d21c0a3be2a532e9a
admin <cyril>
2021-01-11 00:25:39 +0800

    quick_sort.py select_sort.py insert_Sort.py BubbleSort.py

--- quick_sort.py
+++ quick_sort.py
@@ -4,10 +4,18 @@
 @author:cyril
 @file:quick_sort.py
 @time:2021/01/10/23:25
+复杂度 O(nlogn)
 """
 import  random
+import sys
+from cal_time import *
+sys.setrecursionlimit(10000)  #修改递归深度
 
 def partition(li,left,right):
+    ##优化， 找一个随机数与第一个数互换
+    rnd=random.randint(left,right)
+    li[left],li[rnd]=li[rnd],li[left]
+    ###
     tmp=li[left]
     while left<right:
         while left<right and tmp<=li[right]:    #从右面找比tmp小的数的下标
@@ -17,15 +25,23 @@ def partition(li,left,right):
             left+=1                 #把左边界右移一步
         li[right]=li[left]          #把左边的值写到右边
     li[left]=tmp            #tmp归位
+    #print(li)
     return left             #返回归为的位置标
 
-def quick_sort(li,left,right):
-    mid=partition(li,left,right)  #先找到归为的值
-    quick_sort(li,left,mid-1)     #对mid左边的数递归归位
-    quick_sort(li,mid+1,right)    #对mid右边的数递归归位
+def _quick_sort(li,left,right):
+    if left<right :
+        mid=partition(li,left,right)  #先找到归为的值
+        _quick_sort(li,left,mid-1)     #对mid左边的数递归归位
+        _quick_sort(li,mid+1,right)    #对mid右边的数递归归位
+
+@cal_time()
+def quick_sort(li):
+    _quick_sort(li,0,len(li)-1)
 
 if __name__ == '__main__':
-    ilist=[8,9,4,5,6,1,2,3,11]
-    #ilist = list(range(100))
-    random.shuffle(ilist)
-    quick_sort(ilist,0,len(ilist)-1)
+
+    #ilist=[9,8,7,6,5,4,3,2,1]  ##最坏情况
+    ilist = list(range(10000,0,-1))
+    #random.shuffle(ilist)
+    quick_sort(ilist)
+    #print(ilist)
